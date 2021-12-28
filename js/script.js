d2198fa7622f9c7c07a225b2cda4337d08ee752e
AdrianEd50 <73480640+AdrianEd50@users.noreply.github.com>
2021-12-28 11:41:43 +0100

    finished two tasks

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*Practical*/
+
+/*Task 1*/
+var firstNmae = "Adrian";
+var surName = "Lafjell Ed";
+
+var fullName = firstNmae + " " + surName;
+console.log(fullName);
+
+/*Task 2*/
+const house = {
+  type: "apartment",
+
+  rooms: "3",
+
+  material: "wood",
+
+  occupied: true,
+};
+
+console.log([house.type, house.rooms, house.material, house.occupied]);
